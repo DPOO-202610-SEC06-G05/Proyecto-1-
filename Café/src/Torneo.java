d6be8e71df8b795415cf518c32e56f7dd399a7be
Harenn10 <su.gonzalez@uniandes.edu.co>
2026-04-27 22:52:44 -0500

    a

--- Café/src/Torneo.java
+++ Café/src/Torneo.java
@@ -1,3 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 public class Torneo {
-    
+    private Juego juego;
+    private int cuposMaximos;
+    private boolean esAmistoso;
+    private List<Usuario> inscritos;
+    private int cuposFan;
+    private Map<Usuario, Integer> cuposUsuario;
+
+    public Torneo(Juego juego, int cuposMaximos, boolean esAmistoso, int cuposFan){
+        this.juego = juego;
+        this.cuposMaximos = cuposMaximos;
+        this.esAmistoso = esAmistoso;
+        this.cuposFan = (int) Math.ceil(cuposMaximos * 0.20);
+        this.inscritos = new ArrayList<>();
+        this.cuposUsuario = new HashMap<>();
+    }
+
+    public inscribir(){
+
+    }
+
+    public desinscribir(){
+        
+    }
 }
